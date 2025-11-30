9ec54a73bd8002ae96c3d560fbb97d89988ac56b
tlord <kook28515@gmail.com>
2025-11-30 01:28:41 +0100

    fix: remove base path from vite config to fix MIME type error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TAI/',
+
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
